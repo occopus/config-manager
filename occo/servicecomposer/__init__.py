2012215e980290c8c05ce03ef344ce848db9eb33
Jozsef Kovacs <jozsef.kovacs@sztaki.mta.hu>
2015-09-18 14:41:44 +0200

    Adding dry_run capability to SC-dummy methods Adding name attribute for SC-dummy

--- occo/servicecomposer/__init__.py
+++ occo/servicecomposer/__init__.py
@@ -11,6 +11,7 @@
 __all__  = [ 'ServiceComposer' ]
 
 import occo.util.factory as factory
+import occo.util as util
 import occo.infobroker as ib
 import logging
 
@@ -38,15 +39,18 @@ def uid():
 
 @factory.register(ServiceComposer, 'dummy')
 class DummyServiceComposer(ServiceComposer):
-    def __init__(self):
+    def __init__(self, name='dummy'):
+        self.name = name
         self.environments = dict()
         self.node_lookup = dict()
         import occo.infobroker as ib
         self.ib = ib.main_info_broker
         self.lock = threading.RLock()
 
+    @util.wet_method()
     def register_node(self, resolved_node_definition):
-        log.debug("[SC] Registering node: %r", resolved_node_definition['name'])
+        log.debug("[%s] Registering node: %r", 
+                self.name, resolved_node_definition['name'])
         with self.lock:
             infra_id = resolved_node_definition['infra_id']
 
@@ -54,23 +58,25 @@ class DummyServiceComposer(ServiceComposer):
             # (May not be useful for real SCs!)
             if not infra_id in self.environments:
                 log.debug(
-                    '[SC] Implicitly creating an environment %r for node %r',
-                    infra_id, resolved_node_definition['name'])
+                    '[%s] Implicitly creating an environment %r for node %r',
+                    self.name, infra_id, resolved_node_definition['name'])
                 self.create_infrastructure(infra_id)
-
+            
             env = self.environments[infra_id].setdefault(
-                resolved_node_definition['name'], list())
+                    resolved_node_definition['name'], list())
             env.append(resolved_node_definition)
             self.node_lookup[resolved_node_definition['node_id']] = resolved_node_definition
-            log.debug("[SC] Done - '%r'", self)
+            log.debug("[%s] Done - '%r'", self.name, self)
 
+    @util.wet_method()
     def drop_node(self, instance_data):
         node_id = instance_data['instance_id']
         if not node_id in self.node_lookup:
-            log.debug('[SC] drop_node: Node does not exist; skipping.')
+            log.debug('[%s] drop_node: Node does not exist; skipping.',
+                        self.name)
             return
 
-        log.debug("[SC] Dropping node %r", node_id)
+        log.debug("[%s] Dropping node %r", self.name, node_id)
         with self.lock:
             node = self.node_lookup[node_id]
             infra_id = node.infra_id
@@ -78,32 +84,36 @@ class DummyServiceComposer(ServiceComposer):
                 i for i in self.environments[infra_id]
                 if i.id != node_id)
             del self.node_lookup[node_id]
-            log.debug("[SC] Done - '%r'", self)
+            log.debug("[%s] Done - '%r'", self.name, self)
 
+    @util.wet_method()
     def create_infrastructure(self, infra_id):
-        log.debug("[SC] Creating infrastructure %r", infra_id)
+        log.debug("[%s] Creating infrastructure %r", self.name, infra_id)
         with self.lock:
             self.environments.setdefault(infra_id, dict())
-            log.debug("[SC] Done - '%r'", self)
+            log.debug("[%s] Done - '%r'", self.name, self)
 
+    @util.wet_method()
     def drop_infrastructure(self, infra_id):
         if not infra_id in self.environments:
-            log.debug('[SC] drop_infrastructure: Infrastructure does not exist; skipping.')
+            log.debug('[%s] drop_infrastructure: Infrastructure does not exist; skipping.', self.name)
             return
-        log.debug("[SC] Dropping infrastructure %r", infra_id)
+        log.debug("[%s] Dropping infrastructure %r", self.name, infra_id)
         with self.lock:
             del self.environments[infra_id]
-            log.debug("[SC] Done - '%r'", self)
+            log.debug("[%s] Done - '%r'", self.name, self)
 
+    @util.wet_method('ready')
     def get_node_state(self, instance_data):
         node_id = instance_data['node_id']
-        log.debug("[SC] Querying node state for %r", node_id)
+        log.debug("[%s] Querying node state for '%r'", self.name, node_id)
         with self.lock:
             node = self.node_lookup.get(node_id, None)
             state = 'ready' if node else 'unknown'
-            log.debug("[SC] Done - %r", state)
+            log.debug("[%s] Done - %r", self.name, state)
         return state
 
+    @util.wet_method('dummy-value')
     def get_node_attribute(self, node_id, attribute):
         attrspec = attribute \
             if hasattr(attribute, '__iter__') \
@@ -111,6 +121,7 @@ class DummyServiceComposer(ServiceComposer):
         return '{{{{{0}{1}}}}}'.format(node_id,
                                ''.join('[{0}]'.format(i) for i in attrspec))
 
+    @util.wet_method(True)
     def infrastructure_exists(self, infra_id):
         return infra_id in self.environments
 
